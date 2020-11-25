6509d46e44dc513fa9b4de32a04113cd672ee998
glaze <luyiping1011@gmail.com>
2020-11-24 17:11:18 -0800

    lint slotsInfo

--- src/components/SlotsInfo.js
+++ src/components/SlotsInfo.js
@@ -2,57 +2,56 @@ import React from "react";
 import { useOverrides } from "@quarkly/components";
 import { Text, Box } from "@quarkly/widgets";
 const defaultProps = {
-	"quarkly-title": "Slots Static",
-	"display": "flex",
-	"flex-direction": "column",
-	"align-items": "flex-start",
-	"bottom": "16px",
-	"overflow-x": "hidden"
+  "quarkly-title": "Slots Static",
+  display: "flex",
+  "flex-direction": "column",
+  "align-items": "flex-start",
+  bottom: "16px",
+  "overflow-x": "hidden",
 };
 const overrides = {
-	"text": {
-		"kind": "Text",
-		"props": {
-			"color": "#ffffff",
-			"position": "static",
-			"bottom": "24px",
-			"quarkly-title": "slots total number",
-			"font": "--bold16",
-			"margin": "0 0px 4px 0px",
-			"overflow-x": "hidden",
-			"children": "100"
-		}
-	},
-	"text1": {
-		"kind": "Text",
-		"props": {
-			"color": "#ffffff",
-			"position": "static",
-			"bottom": "24px",
-			"quarkly-title": "slots total number",
-			"font": "--thin16",
-			"margin": "0 0px 16px 0px",
-			"overflow-x": "hidden",
-			"children": "Slots Total"
-		}
-	}
+  text: {
+    kind: "Text",
+    props: {
+      color: "#ffffff",
+      position: "static",
+      bottom: "24px",
+      "quarkly-title": "slots total number",
+      font: "--bold16",
+      margin: "0 0px 4px 0px",
+      "overflow-x": "hidden",
+      children: "12",
+    },
+  },
+  text1: {
+    kind: "Text",
+    props: {
+      color: "#ffffff",
+      position: "static",
+      bottom: "24px",
+      "quarkly-title": "slots total number",
+      font: "--thin16",
+      margin: "0 0px 16px 0px",
+      "overflow-x": "hidden",
+      children: "Slots Total",
+    },
+  },
 };
 
-const SlotsInfo = props => {
-	const {
-		override,
-		children,
-		rest
-	} = useOverrides(props, overrides, defaultProps);
-	return <Box {...rest}>
-		<Text {...override("text")} />
-		<Text {...override("text1")} />
-		{children}
-	</Box>;
+const SlotsInfo = (props) => {
+  const { override, children, rest } = useOverrides(
+    props,
+    overrides,
+    defaultProps
+  );
+  return (
+    <Box {...rest}>
+      <Text {...override("text")} />
+      <Text {...override("text1")} />
+      {children}
+    </Box>
+  );
 };
 
-Object.assign(SlotsInfo, { ...Box,
-	defaultProps,
-	overrides
-});
+Object.assign(SlotsInfo, { ...Box, defaultProps, overrides });
 export default SlotsInfo;
