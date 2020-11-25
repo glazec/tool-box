a02add7a039209988b19686b63612dbb255e7b09
glaze <luyiping1011@gmail.com>
2020-11-24 16:19:34 -0800

    add toolcard.js

--- src/components/ToolCard.js
+++ src/components/ToolCard.js
@@ -1,79 +1,162 @@
 import React from "react";
 import { useOverrides } from "@quarkly/components";
-import { Icon, Image, Text, Box } from "@quarkly/widgets";
-import { FaCircle } from "react-icons/fa";
+import { Icon, Image, Text, Box, Link } from "@quarkly/widgets";
 const defaultProps = {
-	"display": "flex",
-	"flex-direction": "column",
-	"padding": "8px 8px 8px 8px",
-	"align-items": "center",
-	"max-width": "150px",
-	"width": "130px",
-	"border-width": "1px",
-	"border-style": "solid",
-	"margin": "0px 16px 16px 0px"
+  display: "block",
+  "flex-direction": "column",
+  padding: "8px 8px 8px 8px",
+  "align-items": "center",
+  "border-style": "solid",
+  margin: "0px 0px 0 0px",
+  width: "auto",
+  "hover-box-shadow": "0 0 0 0 rgba(0, 0, 0, 0.)",
+  "border-width": "2px",
+  "border-color": "#edf0f3",
+  "border-radius": "5px",
+  "hover-border-color": "rgba(47, 107, 255, 0.56)",
+  "hover-border-radius": "15px",
+  "max-width": "386px",
+  "max-height": "386px",
+  "hover-transition": "all 0.8s --transitionTimingFunction-easeOut 0.5s",
+  "grid-column": "auto",
+  position: "relative",
 };
 const overrides = {
-	"icon": {
-		"kind": "Icon",
-		"props": {
-			"category": "fa",
-			"icon": FaCircle,
-			"position": "static",
-			"top": "45px",
-			"right": "492px",
-			"bottom": "auto",
-			"left": "0px",
-			"display": "inline",
-			"align-self": "flex-start",
-			"width": "auto",
-			"height": "auto",
-			"font": "0.3em --fontFamily-googleOpenSans",
-			"size": "8px"
-		}
-	},
-	"image": {
-		"kind": "Image",
-		"props": {
-			"width": "64px",
-			"height": "64px",
-			"src": "https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&w=2000",
-			"display": "inline"
-		}
-	},
-	"text": {
-		"kind": "Text",
-		"props": {
-			"text-align": "left",
-			"children": "Some text"
-		}
-	},
-	"text1": {
-		"kind": "Text",
-		"props": {
-			"children": "Some text",
-			"align-self": "auto"
-		}
-	}
+  icon: {
+    kind: "Icon",
+    props: {
+      category: "fa",
+      icon: "FaCircle",
+      right: "auto",
+      bottom: "auto",
+      display: "inline",
+      "align-self": "flex-start",
+      width: "auto",
+      height: "auto",
+      font: "0.3em --fontFamily-googleOpenSans",
+      size: "8px",
+      position: "absolute",
+      top: "16px",
+      left: "16px",
+    },
+  },
+  box: {
+    kind: "Box",
+    props: {
+      display: "flex",
+      "flex-direction": "column",
+      padding: "8px 8px 8px 8px",
+      "align-items": "center",
+      "border-style": "solid",
+      margin: "0px 0px 0 0px",
+      width: "auto",
+      "hover-box-shadow": "0 0 0 0 rgba(0, 0, 0, 0.)",
+      "border-width": "0px",
+      "border-color": "#edf0f3",
+      "border-radius": "5px",
+      "hover-border-color": "rgba(47, 107, 255, 0.56)",
+      "hover-border-radius": "15px",
+      "max-width": "386px",
+      "max-height": "386px",
+      "hover-transition": "all 0.8s --transitionTimingFunction-easeOut 0.5s",
+      "grid-column": "auto",
+    },
+  },
+  image: {
+    kind: "Image",
+    props: {
+      width: "128px",
+      height: "128px",
+      src:
+        "https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&w=2000",
+      display: "inline",
+    },
+  },
+  text: {
+    kind: "Text",
+    props: {
+      "text-align": "left",
+      font: "--heading2",
+      margin: "8px 0px 8px 0px",
+      children: "Slack",
+    },
+  },
+  text1: {
+    kind: "Text",
+    props: {
+      "align-self": "auto",
+      font: "--thin16",
+      margin: "0 0px 16px 0px",
+      "text-align": "center",
+      children: (
+        <>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringilla urna,
+          porttitor rhoncus{" "}
+        </>
+      ),
+    },
+  },
+  box1: {
+    kind: "Box",
+    props: {
+      display: "flex",
+      "max-width": "100%",
+      "align-self": "flex-end",
+      bottom: "8px",
+      "justify-content": "flex-end",
+      children: (
+        <>
+          <Link
+            href="#"
+            font="12px sans-serif"
+            text-decoration-line="initial"
+            color="rgba(26, 33, 56, 0.46)"
+            hover-color="#1a2138"
+            justify-content="flex-end"
+          >
+            Browser
+          </Link>
+          <Link
+            href="#"
+            margin="0px 0px 0px 8px"
+            font="12px sans-serif"
+            text-decoration-line="initial"
+            color="rgba(26, 33, 56, 0.46)"
+            hover-color="#1a2138"
+          >
+            Python
+          </Link>
+        </>
+      ),
+      top: "auto",
+      position: "absolute",
+      right: "8px",
+    },
+  },
 };
 
-const ToolCard = props => {
-	const {
-		override,
-		children,
-		rest
-	} = useOverrides(props, overrides, defaultProps);
-	return <Box {...rest}>
-		<Icon {...override("icon")} />
-		<Image {...override("image")} />
-		<Text {...override("text")} />
-		<Text {...override("text1")} />
-		{children}
-	</Box>;
+const ToolCard = (props) => {
+  overrides["text"]["props"]["children"] = props.title;
+  overrides["text1"]["props"]["children"] = <>{props.description}</>;
+  overrides["image"]["props"]["src"] = props.imageUrl;
+  const { override, children, rest } = useOverrides(
+    props,
+    overrides,
+    defaultProps
+  );
+
+  return <Box {...rest}>
+      <Icon {...override("icon")} />
+      <Box {...override("box")}>
+        <Image {...override("image")} />
+        <Text {...override("text")} />
+        <Text {...override("text1")} />
+      </Box>
+      <Box {...override("box1")} />
+      {children}
+    </Box>
 };
 
-Object.assign(ToolCard, { ...Box,
-	defaultProps,
-	overrides
-});
+Object.assign(ToolCard, { ...Box, defaultProps, overrides });
 export default ToolCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,115 +4,130 @@ import { Theme, Link, Text, Box, Button, Hr } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { Override } from "@quarkly/components";
 import * as Components from "components";
-import ToolCardGrid from "components/ToolCardGrid"
+import ToolCardGrid from "components/ToolCardGrid";
 
-
-export default (() => {
-	return <Theme theme={theme}>
-		<Helmet>
-			<title>
-				Quarkly export
-			</title>
-			<meta name={"description"} content={"Web site created using quarkly.io"} />
-			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
-		</Helmet>
-		<Box
-			display="flex"
-			padding="0px 0px 0px px"
-			grid-auto-columns="2"
-			grid-auto-rows="1"
-			background="#FFFFFF"
-			align-items="stretch"
-			justify-content="space-between"
-		>
-			<Box
-				background="--backgroundDark"
-				grid-column="1"
-				grid-row="1"
-				quarkly-title="left panel"
-				padding="32px 32px 32px 32px"
-				max-width="150px"
-				flex="1 1 10%"
-				margin="0px 5% 0px 0px"
-			>
-				<Text
-					color="--textWhite"
-					font="--heading2"
-					overflow-x="hidden"
-					quarkly-title="Title"
-					width="auto"
-					margin="0 0px 16px 0px"
-				>
-					Inevitable
-				</Text>
-				<Box position="relative" margin="0px 0px 16 0px" top="95%" bottom="2%">
-					<Components.SlotsInfo bottom={0} top="auto" />
-				</Box>
-			</Box>
-			<Box
-				quarkly-title="main content"
-				grid-column="2"
-				grid-row="1"
-				padding="32px 0px 0px 0px"
-				display="block"
-				justify-self="stretch"
-				flex="1  5 80%"
-			>
-				<Button
-					background="rgba(0, 0, 0, 0)"
-					border-color="#989EAD"
-					border-width="1px"
-					border-style="solid"
-					color="#2F6BFF"
-					border-radius="15px"
-					position="absolute"
-					right="16px"
-					top="22px"
-				>
-					Build your own tool box
-				</Button>
-				<Text
-					color="--textDark"
-					font="--heading2"
-					overflow-x="hidden"
-					quarkly-title="Title"
-					width="auto"
-					margin="0 0px 32px 0px"
-				>
-					Tools
-				</Text>
-		<ToolCardGrid />
-			</Box>
-			<Hr
-				position="absolute"
-				top="66px"
-				width="100%"
-				color="#0000"
-				border-color="#F2F3F5"
-				background="#0000"
-			/>
-		</Box>
-		<Link
-			font={"--capture"}
-			font-size={"10px"}
-			position={"fixed"}
-			bottom={"12px"}
-			right={"12px"}
-			z-index={"4"}
-			border-radius={"4px"}
-			padding={"5px 12px 4px"}
-			background-color={"--dark"}
-			opacity={"0.6"}
-			hover-opacity={"1"}
-			color={"--light"}
-			cursor={"pointer"}
-			transition={"--opacityOut"}
-			quarkly-title={"Badge"}
-			text-decoration-line={"initial"}
-			href={"https://quarkly.io/"}
-			target={"_blank"}
-		>
-			Made on Quarkly
-		</Link>
-	</Theme>;
-});
+export default () => {
+  return (
+    <Theme theme={theme}>
+      <Helmet>
+        <title>Quarkly export</title>
+        <meta
+          name={"description"}
+          content={"Web site created using quarkly.io"}
+        />
+        <link
+          rel={"shortcut icon"}
+          href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"}
+          type={"image/x-icon"}
+        />
+      </Helmet>
+      <Box
+        display="flex"
+        padding="0px 0px 0px px"
+        grid-auto-columns="2"
+        grid-auto-rows="1"
+        background="#FFFFFF"
+        align-items="stretch"
+        justify-content="space-between"
+      >
+        <Box
+          background="--backgroundDark"
+          grid-column="1"
+          grid-row="1"
+          quarkly-title="left panel"
+          padding="32px 32px 32px 32px"
+          max-width="150px"
+          flex="1 1 10%"
+          margin="0px 5% 0px 0px"
+          min-height="100%"
+          position="relative"
+        >
+          <Text
+            color="--textWhite"
+            font="--heading2"
+            overflow-x="hidden"
+            quarkly-title="Title"
+            width="auto"
+            margin="0 0px 16px 0px"
+          >
+            Inevitable
+          </Text>
+          <Box
+            position="absolute"
+            margin="0px 0px 16px 0px"
+            top="95%"
+            bottom="auto"
+          >
+            <Components.SlotsInfo bottom={0} top="auto" />
+          </Box>
+        </Box>
+        <Box
+          quarkly-title="main content"
+          grid-column="2"
+          grid-row="1"
+          padding="32px 32px 0px 0px"
+          display="block"
+          justify-self="stretch"
+          flex="1  5 80%"
+        >
+          <Button
+            background="rgba(0, 0, 0, 0)"
+            border-color="#989EAD"
+            border-width="1px"
+            border-style="solid"
+            color="#2F6BFF"
+            border-radius="15px"
+            position="absolute"
+            right="16px"
+            top="22px"
+            disabled={false}
+            type="button"
+          >
+            Build your own tool box
+          </Button>
+          <Text
+            color="--textDark"
+            font="--heading2"
+            overflow-x="hidden"
+            quarkly-title="Title"
+            width="auto"
+            margin="0 0px 32px 0px"
+          >
+            Tools
+          </Text>
+          <ToolCardGrid />
+        </Box>
+        <Hr
+          position="absolute"
+          top="66px"
+          width="100%"
+          color="#0000"
+          border-color="#F2F3F5"
+          background="#0000"
+        />
+      </Box>
+      <Link
+        font={"--capture"}
+        font-size={"10px"}
+        position={"fixed"}
+        bottom={"12px"}
+        right={"12px"}
+        z-index={"4"}
+        border-radius={"4px"}
+        padding={"5px 12px 4px"}
+        background-color={"--dark"}
+        opacity={"0.6"}
+        hover-opacity={"1"}
+        color={"--light"}
+        cursor={"pointer"}
+        transition={"--opacityOut"}
+        quarkly-title={"Badge"}
+        text-decoration-line={"initial"}
+        href={"https://quarkly.io/"}
+        target={"_blank"}
+      >
+        Made on Quarkly
+      </Link>
+    </Theme>
+  );
+};
